a7fbcfb0f6e80e310c5d8e62c5cc713b9149bd0c
Amal <jomonamal999@gmail.com>
2023-03-23 15:29:56 +0100

    Adds servcie for area

--- Management/src/main/java/it/ripasso/management/service/AreaService.java
+++ Management/src/main/java/it/ripasso/management/service/AreaService.java
@@ -1,2 +1,13 @@
-package it.ripasso.management.service;public interface AreaService {
+package it.ripasso.management.service;
+
+import it.ripasso.management.dto.AreaDto;
+import it.ripasso.management.model.Area;
+
+import java.util.Optional;
+
+public interface AreaService {
+
+    Area save(AreaDto areaDto);
+
+    Optional<Area> findById(Long id);
 }
--- Management/src/main/java/it/ripasso/management/service/AreaServiceImpl.java
+++ Management/src/main/java/it/ripasso/management/service/AreaServiceImpl.java
@@ -1,2 +1,27 @@
-package it.ripasso.management.service;public class AreaServiceImpl {
+package it.ripasso.management.service;
+
+import it.ripasso.management.dto.AreaDto;
+import it.ripasso.management.model.Area;
+import it.ripasso.management.repository.AreaRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+@Service
+public class AreaServiceImpl implements AreaService{
+
+    @Autowired
+    private AreaRepository repo;
+
+    @Override
+    public Area save(AreaDto areaDto) {
+        ModelMapper modelMapper = new ModelMapper();
+        return repo.save(modelMapper.map(areaDto, Area.class));
+    }
+
+    @Override
+    public Optional<Area> findById(Long id) {
+        return repo.findById(id);
+    }
 }
