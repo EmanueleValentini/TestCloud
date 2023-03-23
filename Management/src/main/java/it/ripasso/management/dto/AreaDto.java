4227606b86bb90a0b70c65b20ce1b7d2d7af96cc
Amal <jomonamal999@gmail.com>
2023-03-23 12:47:02 +0100

    added dto area

---
+++ Management/src/main/java/it/ripasso/management/dto/AreaDto.java
@@ -0,0 +1,22 @@
+package it.ripasso.management.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class AreaDto {
+
+    private String code;
+
+    private Boolean activeFlag;
+
+
+}
