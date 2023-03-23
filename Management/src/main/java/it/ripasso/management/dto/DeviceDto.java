6a407fff298adc3e6996d9183c0137332d329dee
EmanueleValentini <emanuele_valentini@hotmail.it>
2023-03-23 12:44:53 +0100

    Added DeviceDto

---
+++ Management/src/main/java/it/ripasso/management/dto/DeviceDto.java
@@ -0,0 +1,17 @@
+package it.ripasso.management.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DeviceDto {
+
+    private String code;
+    
+    private boolean activeFlag;
+}
